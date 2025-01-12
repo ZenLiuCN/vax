a44860f086f1ec65046764e0e3fb2f505e5ff19e
Zen.Liu <lcz20@163.com>
2025-01-12 20:17:07 +0800

    update structure
    Took 44 seconds

--- common/src/main/java/vax/common/XEntity.java
+++ common/src/main/java/vax/common/XEntity.java
@@ -6,7 +6,7 @@ package vax.common;
  * @author Zen.Liu
  * @since 2024-10-01
  */
-public interface XEntity extends XRecord {
+public interface XEntity extends XPersis {
     @Override
     Class<? extends XEntity> $type();
 }
